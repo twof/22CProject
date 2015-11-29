a5d0a7ecc9b97a0ef26302ed113d3287e42a80d3
twof <fabiobean2@gmail.com>
2015-11-28 23:31:43 -0800

    Final Changes on this branch

--- Tests.cpp
+++ Tests.cpp
@@ -8,6 +8,7 @@ int main() {
     OMRA omra;
     omra.addNewCountry("Alisa", 1000, 200, 500);
     omra.addNewCountry("Alex", 2000, 300, 10);
+    omra.addNewCountry("Murica", 5000, 300, 30);
     omra.printByCountry();
     cout << "Print by rank: " << endl;
     omra.printByRank();
